b1ffb89377161a64e5bfda582b5c5bc2982e6eb5
Mohamed <mohamedbenjdira5@gmail.com>
2022-07-19 02:19:59 +0100

    Math Feature

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ document.querySelector(".date").textContent = date;
 let budget = document.querySelector(".budget");
 const btn = document.querySelector(".add_btn");
 
+// document.querySelector("select").onchange = (event) => {
+//   let type =
+//     document.querySelector(".add_type")[
+//       document.querySelector(".add_type").selectedIndex
+//     ].textContent;
+//   if (type == "+") {
+//     event.currentTarget.classList.remove("focus:border-red-500");
+//     event.currentTarget.classList.add("focus:border-mainBlue");
+//   } else {
+//     event.currentTarget.classList.remove("focus:border-mainBlue");
+//     event.currentTarget.classList.add("focus:border-red-500");
+//   }
+// };
+
 //Event Handler
 btn.addEventListener("click", (event) => {
   let type =
@@ -20,7 +34,7 @@ btn.addEventListener("click", (event) => {
     type == "+" ? "plus" : "minus"
   }">${type} ${val} <span class="percent ${
     type == "+" ? "hidden" : "inline-block"
-  }">5%</span>`;
+  }"></span>`;
   if (type == "+") document.querySelector(".income_section").append(content);
   else document.querySelector(".expenses_section").append(content);
   check_bg(type);
@@ -28,6 +42,7 @@ btn.addEventListener("click", (event) => {
     `${type == "+" ? ".curr_income" : ".curr_expenses"}`
   ).firstChild.textContent = `${type} ${check_money(type)}`;
   updateBudget();
+  updatePercent();
 });
 
 function check_bg(sign) {
@@ -57,4 +72,22 @@ function check_money(sign) {
 function updateBudget() {
   let diff = check_money("+") - check_money("-");
   budget.textContent = `${diff >= 0 ? "+" : "-"} ${Math.abs(diff).toFixed(2)}`;
+}
+
+function updatePercent() {
+  let expense_content = document
+    .querySelector(".expenses_section")
+    .querySelectorAll(".content");
+  expense_content.forEach((ele) => {
+    let val = +ele.firstElementChild.firstChild.textContent.slice(2);
+    if (check_money("+") != 0)
+      ele.firstElementChild.firstElementChild.textContent = `${(
+        (val / check_money("+")) *
+        100
+      ).toFixed(2)} %`;
+  });
+  document.querySelector(".curr_expenses").firstElementChild.textContent = `${(
+    (check_money("-") / check_money("+")) *
+    100
+  ).toFixed(2)} %`;
 }
